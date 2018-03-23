6fd315acc225a25419b71b86ab5ecd09ab40fec7
Alelia40 <35640542+Alelia40@users.noreply.github.com>
2018-03-22 20:09:52 -0400

    ;akjfha;kjhf

--- king.java
+++ king.java
@@ -6,15 +6,12 @@ public class king extends piece{
     super(x,y);
   }
  
-  
-  //hello
+  //method to check if the king has been moved
    public Boolean getMoved(){
     return hasMoved;
   }
   
-  //all methods except for move are inherited
-  
-  
+  //all methods except for move are inherited, move behavior changed
   @Override
   public void move(int x, int y){
     System.out.println("placeholder");
