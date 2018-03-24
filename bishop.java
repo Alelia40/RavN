98777282dd48292ff1c74901f45ce40143442ca1
Alelia40 <35640542+Alelia40@users.noreply.github.com>
2018-03-23 21:11:51 -0400

    Piece motion v1
    Adds basic traversal motion within constraints of piece rules. Has not yet incorporated checking for obstructions or taking pieces to that motion

--- bishop.java
+++ bishop.java
@@ -1,19 +1,31 @@
 public class bishop extends piece{
   
-  public bishop(int x, int y){
-    super(x,y);
+  public bishop(int x, int y, int plr){
+    super(x,y,plr);
   }
   
-  //all methods except for move are inherited, move behavior changed
+  //all methods from piece except for move are inherited, move behavior changed
   @Override
   public void move(int x, int y){
-    System.out.println("placeholder");
+    
+    if(validMove(x,y) == false){
+      //do some sort of error behavior
+    }
+    else{
+       super.move(x,y);
+    }
     
   }
   
-  
-  
-  
-  
-  
+  //helper method to get the distance of any position from the current position
+  private boolean validMove(int x, int y){
+    int distA = Math.abs(this.getX() - x); 
+    int distB = Math.abs(this.getY() - y);
+    
+    //if the destination has an equal distance in terms of x and y axes from the origin, then it is diagonal and a valid move
+    if(distA == distB)
+      return true;
+    else
+      return false;
+  }
 }
