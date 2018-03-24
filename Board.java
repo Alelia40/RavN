1819e987c363aca673ba31456475cc2346f25c2b
Alelia40 <35640542+Alelia40@users.noreply.github.com>
2018-03-24 00:22:00 -0400

    BoardUI
    Got BoardUI working, haven't added actions to buttons yet or dealt with placing pieces

--- Board.java
+++ Board.java
@@ -3,7 +3,6 @@ import java.awt.*;
 
 public class Board extends JFrame{
   
-  private JPanel board;
   
   private tile[][] tiles;
   
@@ -15,21 +14,25 @@ public class Board extends JFrame{
     catch (Exception e) {
     }
     
-    this.board = new JPanel(new GridLayout(8,8));
-    this.tiles = new tile[8][8];     //creates as many tiles as there are spaces on the board
-    Container c = this.getContentPane();                //formats the JFrame to have the grid of game tiles over the information of who's turn it is 
-    c.add(this.board);      
+    Container c = this.getContentPane();                //formats the JFrame to have the grid of game tiles over the information of who's turn it is
+    this.setSize(800,800);
+    this.tiles = new tile[8][8];     //creates as many tiles as there are spaces on the board 
+    c.setLayout(new GridLayout(8,8)); //create a gridlayout on the container     
     
-    
+    int count = 1;
     for (int index1 = 0 ; index1 < 8 ; index1++){
       for (int index2 = 0 ; index2 < 8 ; index2++){
-        tiles[index2][index1] = new tile(true);
-      }
+        tiles[index2][index1] = new tile();
+        if((index1+index2)%2 == 1)
+          tiles[index2][index1].setBackground(new Color(176,160,77));
+        else
+          tiles[index2][index1].setBackground(new Color(90,77,14));
+        c.add(tiles[index2][index1]);
     }
     
     this.setVisible(true);
     
  
   }
-  
+  }
 }
